b95a825f6b0f91f8302a028496ae94e0ee9d3e42
pp449 <mma7710@naver.com>
2023-05-07 15:19:51 +0900

    config: dotenv-vault 설정 및 환경변수 모듈 추가

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { sum } from '@apis/index';
+import { env } from 'process';
+
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
-
-console.log(sum(1, 3));
+app.use(morgan('dev'));
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello');
 });
 
-app.listen('8080', () => {
+app.listen(env.PORT, () => {
   console.log('서버 실행중');
 });
