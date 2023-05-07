0b4e0d554894f499b7a2468394d83979a2f4f6f9
pp449 <mma7710@naver.com>
2023-05-07 15:25:12 +0900

    Feat: cors 설정 추가

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { env } from 'process';
 
+import { corsOptions } from '@middlewares/cors';
+import cors from 'cors';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors(corsOptions));
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello');
