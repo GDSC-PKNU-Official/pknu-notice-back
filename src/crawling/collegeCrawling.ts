2faad62a9bee2ae3adb0e326a1bda5b1bd4e6a90
pp449 <mma7710@naver.com>
2023-05-08 18:00:48 +0900

    Feat: 단과대 크롤링 추가

--- src/crawling/collegeCrawling.ts
+++ src/crawling/collegeCrawling.ts
@@ -1,7 +1,23 @@
-export const collegeCrawling = (): string[] => {
-  const collegeList = [];
-  collegeList.push('경영대학');
-  collegeList.push('공과대학');
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 
+interface College {
+  collegeName: string;
+  collegeLink: string;
+}
+
+export const collegeCrawling = async (): Promise<College[]> => {
+  const collegeList: College[] = [];
+  const PKNUURL = 'https://www.pknu.ac.kr';
+  const response = await axios.get(PKNUURL + '/main/23');
+  const $ = cheerio.load(response.data);
+  $('.subMenu')
+    .find('li')
+    .each((i, li) => {
+      const collegeName = $(li).text().trim();
+      const collegeLink = PKNUURL + $(li).find('a').attr('href');
+      collegeList.push({ collegeName, collegeLink });
+    });
+  console.log(collegeList);
   return collegeList;
 };
