deee1e60fc1a4be41d01d7e2e4b13228897bb93d
pp449 <mma7710@naver.com>
2023-05-14 19:01:19 +0900

    Feat: 학과 졸업요건 링크 크롤링 추가

---
+++ src/crawling/graduation.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+export const graduationRequirements = async (departmentLink: string) => {
+  if (departmentLink.endsWith('/'))
+    departmentLink = departmentLink.slice(0, -1);
+  const graduationLink: string[] = [];
+  const response = await axios.get(departmentLink);
+  const $ = cheerio.load(response.data);
+  const targetName = '졸업요건';
+  const selector = `:contains("${targetName}")`;
+  const targetElements = $(selector);
+
+  targetElements.each((index, element) => {
+    const link = $(element).attr('href');
+    if (link !== undefined) {
+      const URL = departmentLink + link;
+      console.log(URL);
+      graduationLink.push(URL);
+    }
+  });
+
+  return graduationLink;
+};
