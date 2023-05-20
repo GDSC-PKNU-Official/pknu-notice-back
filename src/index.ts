a95dec08aaa4b3d6cf6f1583754494b75d7d813b
pp449 <mma7710@naver.com>
2023-05-20 18:23:42 +0900

    Chore: 크롤링시 TLS 문제 해결

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { env } from 'process';
 
+import { collegeCrawling } from '@crawling/collegeCrawling';
 import { corsOptions } from '@middlewares/cors';
 import errorHandler from '@middlewares/error-handler';
 import cors from 'cors';
@@ -9,6 +10,7 @@ import morgan from 'morgan';
 const app = express();
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 app.get('/test', (req: Request, res: Response) => {
   res.send('Hello');
@@ -19,3 +21,13 @@ app.use(errorHandler);
 app.listen(env.PORT, () => {
   console.log('서버 실행중');
 });
+
+// const start = async () => {
+//   const val = await collegeCrawling();
+//   console.log(val);
+// };
+
+// start();
+
+// // const val3 = graduationRequirementsCrawling('http://safe.pknu.ac.kr');
+// // console.log(val3);
