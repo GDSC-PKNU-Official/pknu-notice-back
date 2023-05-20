cb8cd8540beba0e8442ccc9943178f1d58809aad
pp449 <mma7710@naver.com>
2023-05-20 12:20:35 +0900

    Test: 현재 테스트는 동작하지 않도록 설정

--- src/tests/graduationRequirements.test.ts
+++ src/tests/graduationRequirements.test.ts
@@ -1,8 +1,20 @@
-import { graduationRequirements } from '@crawling/graduation';
+import { collegeCrawling } from '@crawling/collegeCrawling';
+import { departmentListCrawling } from '@crawling/departmentsCrawling';
+import { graduationRequirementsCrawling } from '@crawling/graduationRequirementsCrawling';
 
-test('졸업요건 크롤링 테스트', () => {
-  const graduationRequirementsList: string[] = graduationRequirements();
-  graduationRequirementsList.forEach((element) => {
-    expect(element).toMatch(/http:\/\//);
+test.skip('졸업요건 크롤링 테스트', async () => {
+  const collegeList = await collegeCrawling();
+  collegeList.forEach(async (college) => {
+    const departmentsList = await departmentListCrawling(college.collegeLink);
+    departmentsList.forEach(async (department) => {
+      const result = await graduationRequirementsCrawling(
+        department.departmentLink,
+      );
+      console.log(result);
+      if (result.length !== 1) {
+        console.log(department.department);
+      }
+      expect(result).toHaveLength(1);
+    });
   });
 });
