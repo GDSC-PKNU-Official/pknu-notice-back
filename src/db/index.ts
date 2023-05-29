6f21bb2f9cd6e3131284da44dda584030a65e1c8
pp449 <mma7710@naver.com>
2023-05-29 12:38:16 +0900

    Feat: DB 모듈 추가

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import env from '@config/index';
+import mysql from 'mysql2';
+
+const db = mysql.createConnection({
+  host: 'database',
+  user: env.DB_USER,
+  password: env.DB_PW,
+  database: env.DB_NAME,
+});
+
+db.connect((error) => {
+  if (error) {
+    console.error('MySQL 연결 실패:', error);
+  } else {
+    console.log('MySQL 연결 성공!');
+  }
+});
+
+export default db;
