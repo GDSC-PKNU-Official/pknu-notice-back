999d9d82ec10f745bbf4211fff60deefdedf6201
pp449 <mma7710@naver.com>
2023-06-03 23:32:19 +0900

    Feat: 학과별 테이블 생성하는 모듈 따로 제작

--- sqls/createTable.sql
+++ sqls/createTable.sql
@@ -1,15 +1,5 @@
 USE burimidb;
 
-CREATE TABLE notices (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    major VARCHAR(255) NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    graduate VARCHAR(255),
-    link VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    uploadDate VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     collegeName VARCHAR(255) NOT NULL,
