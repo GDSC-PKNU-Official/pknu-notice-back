6e07c6286a941ccacaf99f387f2ed4ebc5b264b5
pp449 <mma7710@naver.com>
2023-06-04 00:08:56 +0900

    Feat: 공지사항을 DB 에 추가하는 모듈 추가

--- src/db/data/handler.ts
+++ src/db/data/handler.ts
@@ -1,9 +1,13 @@
-import { collegeCrawling } from '@crawling/collegeCrawling';
+import {
+  noticeContentCrawling,
+  noticeCrawling,
+  noticeListCrawling,
+} from '@crawling/noticeCrawling';
+import { College, Notice } from 'src/@types/college';
 import db from 'src/db';
 
-export const saveDepartmentToDB = async () => {
-  const response = await collegeCrawling();
-  for (const data of response) {
+export const saveDepartmentToDB = async (college: College[]) => {
+  for (const data of college) {
     const saveCollegeQuery = `INSERT INTO departments (collegeName, departmentName, departmentSubName, departmentLink) VALUES ('${data.collegeName}', '${data.departmentName}', '${data.departmentSubName}', '${data.departmentLink}');`;
 
     db.query(saveCollegeQuery, (error) => {
@@ -14,4 +18,55 @@ export const saveDepartmentToDB = async () => {
       }
     });
   }
+};
+
+const saveNotice = (notice: Notice, major: string) => {
+  const saveNoticeQuery =
+    'INSERT INTO ' +
+    major +
+    ' (major, title, link, content, uploadDate) VALUES (?, ?, ?, ?, ?)';
+  const values = [
+    major,
+    notice.title,
+    notice.path,
+    notice.description,
+    notice.date,
+  ];
+
+  db.query(saveNoticeQuery, values, (error) => {
+    if (error) {
+      console.error('데이터 입력 실패', error);
+    } else {
+      console.log('공지사항 입력 성공!');
+    }
+  });
+};
+
+export const saveNoticeToDB = async () => {
+  const selectQuery = 'SELECT * FROM departments';
+  db.query(selectQuery, async (error, results) => {
+    if (error) {
+      console.error('SELECT 오류:', error);
+    } else {
+      for (const row of results as College[]) {
+        const college: College = {
+          collegeName: row.collegeName,
+          departmentName: row.departmentName,
+          departmentSubName: row.departmentSubName,
+          departmentLink: row.departmentLink,
+        };
+
+        const noticeLink = await noticeCrawling(college);
+        const noticeList = await noticeListCrawling(noticeLink);
+        for (const notice of noticeList) {
+          const result = await noticeContentCrawling(notice);
+          const major =
+            college.departmentSubName === '-'
+              ? college.departmentName
+              : college.departmentSubName;
+          saveNotice(result, major);
+        }
+      }
+    }
+  });
 };
