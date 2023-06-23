ffbc4d971e723d000e33b8c1e3f9286339933e80
pp449 <mma7710@naver.com>
2023-06-23 11:45:23 +0900

    Feat: 학교 공지사항 테이블을 생성하도록 추가

--- sqls/createTable.sql
+++ sqls/createTable.sql
@@ -7,3 +7,11 @@ CREATE TABLE departments (
     departmentSubName VARCHAR(255) NOT NULL,
     departmentLink VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE schoolnotices {
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    uploadDate VARCHAR(255) NOT NULL,
+}
