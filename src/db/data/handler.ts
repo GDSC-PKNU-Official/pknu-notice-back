332c14760a5b5b36fbd7bd159eaf0d034fd94c31
pp449 <mma7710@naver.com>
2023-07-03 22:26:37 +0900

    Refactor: 학과 공지사항 크롤링 후 DB에 넣는 함수 비동기 함수로 변경

--- src/db/data/handler.ts
+++ src/db/data/handler.ts
@@ -9,7 +9,6 @@ import db from 'src/db';
 export const saveDepartmentToDB = async (college: College[]): Promise<void> => {
   const saveCollegePromises = college.map((data) => {
     const saveCollegeQuery = `INSERT INTO departments (collegeName, departmentName, departmentSubName, departmentLink) VALUES ('${data.collegeName}', '${data.departmentName}', '${data.departmentSubName}', '${data.departmentLink}');`;
-
     return new Promise<void>((resolve, reject) => {
       db.query(saveCollegeQuery, (error) => {
         if (error) {
@@ -30,7 +29,7 @@ export const saveDepartmentToDB = async (college: College[]): Promise<void> => {
   }
 };
 
-const saveNotice = (notice: Notice, major: string) => {
+const saveNotice = (notice: Notice, major: string): Promise<void> => {
   const saveNoticeQuery =
     'INSERT INTO ' +
     major +
@@ -43,50 +42,68 @@ const saveNotice = (notice: Notice, major: string) => {
     notice.date,
   ];
 
-  db.query(saveNoticeQuery, values, (error) => {
-    if (error) {
-      console.error('데이터 입력 실패', error);
-    } else {
-      console.log('공지사항 입력 성공!');
-    }
+  return new Promise((resolve, reject) => {
+    db.query(saveNoticeQuery, values, (error) => {
+      if (error) {
+        console.error('데이터 입력 실패', error);
+        reject(error);
+      } else {
+        console.log('공지사항 입력 성공!');
+        resolve();
+      }
+    });
   });
 };
 
-export const saveNoticeToDB = async () => {
-  const selectQuery = 'SELECT * FROM departments';
-  db.query(selectQuery, async (error, results) => {
-    if (error) {
-      console.error('SELECT 오류:', error);
-    } else {
-      for (const row of results as College[]) {
-        const college: College = {
-          collegeName: row.collegeName,
-          departmentName: row.departmentName,
-          departmentSubName: row.departmentSubName,
-          departmentLink: row.departmentLink,
-        };
-        console.log(college.departmentName);
-
-        const noticeLink = await noticeCrawling(college);
-        const noticeLists = await noticeListCrawling(noticeLink);
-        const major =
-          college.departmentSubName === '-'
-            ? college.departmentName
-            : college.departmentSubName;
-
-        if (noticeLists.pinnedNotice !== undefined) {
-          for (const notice of noticeLists.pinnedNotice) {
-            const result = await noticeContentCrawling(notice);
-            saveNotice(result, major + '고정');
-          }
+export const saveNoticeToDB = async (): Promise<void> => {
+  const selectQuery = 'SELECT * FROM departments;';
+  try {
+    const results = (await new Promise((resolve, reject) => {
+      db.query(selectQuery, (error, results) => {
+        if (error) {
+          console.error('SELECT 오류:', error);
+          reject(error);
+        } else {
+          if (typeof results === 'string') resolve(results);
+          reject('타입 에러');
         }
-        for (const notice of noticeLists.normalNotice) {
+      });
+    })) as College[];
+
+    const savePromises: Promise<void>[] = [];
+
+    for (const row of results) {
+      const college: College = {
+        collegeName: row.collegeName,
+        departmentName: row.departmentName,
+        departmentSubName: row.departmentSubName,
+        departmentLink: row.departmentLink,
+      };
+
+      const noticeLink = await noticeCrawling(college);
+      const noticeLists = await noticeListCrawling(noticeLink);
+      const major =
+        college.departmentSubName === '-'
+          ? college.departmentName
+          : college.departmentSubName;
+
+      if (noticeLists.pinnedNotice !== undefined) {
+        for (const notice of noticeLists.pinnedNotice) {
           const result = await noticeContentCrawling(notice);
-          saveNotice(result, major + '일반');
+          savePromises.push(saveNotice(result, major + '고정'));
         }
       }
+
+      for (const notice of noticeLists.normalNotice) {
+        const result = await noticeContentCrawling(notice);
+        savePromises.push(saveNotice(result, major + '일반'));
+      }
     }
-  });
+
+    await Promise.all(savePromises);
+  } catch (error) {
+    console.error('에러 발생:', error);
+  }
 };
 
 const saveSchoolNotice = async (notices: string[], mode: string) => {
