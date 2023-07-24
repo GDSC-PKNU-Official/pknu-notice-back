80a8c2009076f03522ca34c147370d7af8584d41
Lee sang Yeop <mma7710@naver.com>
2023-07-24 20:40:56 +0900

    Refactor: index에서 15초후 엔드포인트 연결하는 코드 롤백

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import majorRouter from '@apis/majorDecision/controller';
+import noticeRouter from '@apis/notice/controller';
+import suggestionRouter from '@apis/suggestion/controller';
 import env from '@config';
 import { corsOptions } from '@middlewares/cors';
 import errorHandler from '@middlewares/error-handler';
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import { initialCrawling } from 'src/hooks/startCrawlingData';
 
 const app = express();
 app.use(morgan('dev'));
@@ -12,23 +16,11 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 app.use(express.json());
 app.use(errorHandler);
 
-setTimeout(() => {
-  import('src/hooks/startCrawlingData').then((crawling) => {
-    crawling.initialCrawling();
-  });
+initialCrawling();
 
-  import('@apis/majorDecision/controller').then((majorRouter) => {
-    app.use('/api/majorDecision', majorRouter.default);
-  });
-
-  import('@apis/suggestion/controller').then((suggestionRouter) => {
-    app.use('/api/suggestion', suggestionRouter.default);
-  });
-
-  import('@apis/notice/controller').then((noticeRouter) => {
-    app.use('/api/announcement', noticeRouter.default);
-  });
-}, 15000);
+app.use('/api/suggestion', suggestionRouter);
+app.use('/api/majorDecision', majorRouter);
+app.use('/api/announcement', noticeRouter);
 
 app.get('/test', (req: Request, res: Response) => {
   console.log('test');
