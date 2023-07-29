20f5a364239549afc1ba350df790d05b166c50c9
Lee sang Yeop <mma7710@naver.com>
2023-07-29 15:36:06 +0900

    Feat: announement 엔드포인트에 쿼리문이 없는경우 학교 공지사항 반환하도록 추가

--- src/apis/notice/controller.ts
+++ src/apis/notice/controller.ts
@@ -1,14 +1,16 @@
-import { getNotices } from '@apis/notice/service';
+import { getNotices, getSchoolNotices } from '@apis/notice/service';
 import express, { Request, Response } from 'express';
 
 const router = express.Router();
 router.get('/', async (req: Request, res: Response) => {
   try {
     const major = req.query.major as string;
+    let notices;
     if (major === undefined) {
-      res.status(404).json('학과를 입력해주세요');
+      notices = await getSchoolNotices();
+    } else {
+      notices = await getNotices(major);
     }
-    const notices = await getNotices(major);
     res.json(notices);
   } catch (err) {
     console.log(err);
