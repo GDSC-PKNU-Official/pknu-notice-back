3775ec5b3905f04104dbef3c628e6d0e987e1dd6
Lee sang Yeop <mma7710@naver.com>
2023-07-29 15:35:00 +0900

    Feat: 학교 공지사항을 DB 에서 가져와 반환하는 로직 추가

--- src/apis/notice/service.ts
+++ src/apis/notice/service.ts
@@ -1,24 +1,37 @@
 import db from '@db/index';
 import { Notice } from 'src/@types/college';
 
+const getNoticesFromTable = (tableName: string) => {
+  return new Promise<Notice[]>((resolve, reject) => {
+    const getNoticesQuery = `SELECT * FROM ${tableName}`;
+    db.query(getNoticesQuery, (err: Error, res: Notice[]) => {
+      if (err) reject(err);
+      if (res !== undefined && res.length > 0) resolve(res);
+      resolve([]);
+    });
+  });
+};
+
 export const getNotices = async (department: string): Promise<Notice[]> => {
   const notices: Notice[] = [];
 
-  const getNoticesFromTable = (tableName: string) => {
-    return new Promise<void>((resolve, reject) => {
-      const getNoticesQuery = `SELECT * FROM ${tableName}`;
-      db.query(getNoticesQuery, (err: Error, res: Notice[]) => {
-        if (err) reject(err);
-        if (res !== undefined && res.length > 0) notices.push(...res);
-        resolve();
-      });
-    });
-  };
-
-  await Promise.all([
+  const [fixNotices, normalNotices] = await Promise.all([
     getNoticesFromTable(`${department}고정`),
     getNoticesFromTable(`${department}일반`),
   ]);
 
+  notices.push(...fixNotices, ...normalNotices);
+  return notices;
+};
+
+export const getSchoolNotices = async (): Promise<Notice[]> => {
+  const notices: Notice[] = [];
+
+  const [fixNotices, normalNotices] = await Promise.all([
+    getNoticesFromTable('학교고정'),
+    getNoticesFromTable('학교일반'),
+  ]);
+
+  notices.push(...fixNotices, ...normalNotices);
   return notices;
 };
