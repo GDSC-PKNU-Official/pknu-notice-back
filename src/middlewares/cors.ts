3f35e6eec628c2966104e7d6f0f5e089db73a649
hwinkr <dnddl8280@naver.com>
2023-08-12 16:51:29 +0900

    Config: cors 설정에 LOCAL_CLIENT_PATH 추가

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,7 @@
 import { env } from 'process';
 
 export const corsOptions = {
-  origin: env.CLIENT_PATH,
+  origin: [env.CLIENT_PATH, env.LOCAL_CLIENT_PATH],
   method: ['GET', 'POST'],
   allowedHeaders: ['content-Type', 'Authorization'],
 };
