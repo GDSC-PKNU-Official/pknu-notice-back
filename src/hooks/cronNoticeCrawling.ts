a5eaea9eff81ed0109e83384a2ce5cced9a0594a
Lee sang Yeop <mma7710@naver.com>
2023-08-24 15:12:41 +0900

    Feat: 매일 10시에 크롤링 후 유저에게 알림을 보내도록 기능 추가

--- src/hooks/cronNoticeCrawling.ts
+++ src/hooks/cronNoticeCrawling.ts
@@ -1,13 +1,23 @@
+import { pushNotification } from '@apis/subscribe/service';
 import { saveNoticeToDB } from '@db/data/handler';
 import cron from 'node-cron';
 import notificationToSlack from 'src/hooks/notificateToSlack';
 
-cron.schedule('0 2 * * *', async () => {
-  await saveNoticeToDB();
+const pushToUsers = async (majors: string[]) => {
+  return new Promise<void>((resolve) => {
+    for (const major of majors) {
+      pushNotification(major);
+    }
+  });
+};
+
+cron.schedule('0 10 * * *', async () => {
+  const majors = await saveNoticeToDB();
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
   const day = today.getDate();
   notificationToSlack(`${year}-${month}-${day} 크롤링 완료`);
   console.log(`${year}-${month}-${day} 크롤링 완료`);
+  pushToUsers(majors);
 });
