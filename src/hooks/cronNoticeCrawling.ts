f9eefaf64e667fa3daa8a030a280c56a6b487857
Lee sang Yeop <mma7710@naver.com>
2023-10-01 18:03:47 +0900

    Feat: 새로 추가된 공지사항 갯수만큼 push 알림 전송 및 공지사항 제목도 함께 전송하도록 추가

--- src/hooks/cronNoticeCrawling.ts
+++ src/hooks/cronNoticeCrawling.ts
@@ -1,5 +1,6 @@
 import { pushNotification } from '@apis/subscribe/service';
 import {
+  PushNoti,
   saveNoticeToDB,
   saveSchoolNoticeToDB,
   saveWhalebeToDB,
@@ -7,18 +8,17 @@ import {
 import cron from 'node-cron';
 import notificationToSlack from 'src/hooks/notificateToSlack';
 
-const pushToUsers = async (majors: string[]) => {
+const pushToUsers = async (pushNotiToUserLists: PushNoti) => {
   let pushedUserCount = ``;
-  for (const major of majors) {
-    const count = await pushNotification(major);
-    pushedUserCount += `${major} ${count}명 알림 완료\n`;
+  for (const key in pushNotiToUserLists) {
+    const count = await pushNotification(key, pushNotiToUserLists[key]);
+    pushedUserCount += `${key} ${count}명 알림 완료\n`;
   }
   notificationToSlack(pushedUserCount);
 };
 
 cron.schedule('0 0-9 * * 1-5', async () => {
-  const majors = await saveNoticeToDB();
-  await saveNoticeToDB();
+  const pushNotiToUserLists = await saveNoticeToDB();
   await saveSchoolNoticeToDB();
   await saveWhalebeToDB();
   const today = new Date();
@@ -27,5 +27,5 @@ cron.schedule('0 0-9 * * 1-5', async () => {
   const day = today.getDate();
   notificationToSlack(`${year}-${month}-${day} 크롤링 완료`);
   console.log(`${year}-${month}-${day} 크롤링 완료`);
-  pushToUsers(majors);
+  pushToUsers(pushNotiToUserLists);
 });
