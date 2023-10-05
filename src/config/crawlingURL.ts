deab9dcffd1b2bec0314de3fa24ceaabd87f4571
Lee sang Yeop <mma7710@naver.com>
2023-10-05 15:25:43 +0900

    Config: 크롤링할 URL을 다루는 상수 추가

---
+++ src/config/crawlingURL.ts
@@ -0,0 +1,6 @@
+export const PKNU_URL = {
+  LANGUAGE_NOTICE1:
+    'https://www.pknu.ac.kr/main/163?pageIndex=1&bbsId=2&searchCondition=title&searchKeyword=교환학생&cd=',
+  LANGUAGE_NOTICE2:
+    'https://www.pknu.ac.kr/main/163?pageIndex=1&bbsId=2&searchCondition=title&searchKeyword=어학연수&cd=',
+};
