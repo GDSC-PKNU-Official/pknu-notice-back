79ca1cf68b9248b531633ffc127b4bf906aec83e
Lee sang Yeop <mma7710@naver.com>
2023-11-02 02:02:44 +0900

    Fix: 인터페이스 타입을 DB 컬럼과 동일하게 구성

--- src/@types/college.ts
+++ src/@types/college.ts
@@ -1,9 +1,19 @@
 export interface College {
+  id?: number;
   collegeName: string;
   departmentName: string;
   departmentSubName: string;
   departmentLink: string;
   graduationLink?: string;
+}
+
+export interface MajorNotices {
+  title: string;
+  path: string;
+  description: string;
+  upload_date: string;
+  rep_yn: string;
+  department_id?: number;
 }
 
 export interface Notice {
