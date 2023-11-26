236d66ecae725a8b93d7a5e7304c1bdaf77836ef
Lee sang Yeop <mma7710@naver.com>
2023-11-26 16:16:31 +0900

    feat(languageHandler): 어학 관련 공지사항 리팩토링 DB 처리를 콜백 -> async/await 방식으로 변경 변경된 DB에 처리할 수 있도록 쿼리문 변경

--- src/db/data/languageHandler.ts
+++ src/db/data/languageHandler.ts
@@ -4,12 +4,35 @@ import {
 } from '@crawling/noticeCrawling';
 import db from '@db/index';
 import { selectQuery } from '@db/query/dbQueryHandler';
+import { Notices } from 'src/@types/college';
 import { PKNU_URL } from 'src/config/crawlingURL';
 import notificationToSlack from 'src/hooks/notificateToSlack';
 
 interface NotiLink {
   link: string;
 }
+
+const saveNotice = async (result: Notices): Promise<boolean> => {
+  const query =
+    'INSERT INTO notices (title, link, upload_date, author, rep_yn, category) VALUES (?,?,?,?,?,?)';
+  const values = [
+    result.title,
+    result.link,
+    result.upload_date,
+    result.author ? result.author : '글로벌어학교육센터',
+    false,
+    'LANGUAGE',
+  ];
+
+  try {
+    await db.execute(query, values);
+    console.log('어학 공지 입력 성공!');
+    return true;
+  } catch (error) {
+    notificationToSlack(error.message + '어학 공지 입력 실패');
+    return false;
+  }
+};
 
 export const saveLanguageNoticeToDB = async () => {
   const languageNotiLists1 = await noticeListCrawling(
@@ -28,7 +51,7 @@ export const saveLanguageNoticeToDB = async () => {
 
   const newNoticeTitle: string[] = [];
 
-  const showDBQuery = `SELECT link FROM notices WHERE category = LANGUAGE;`;
+  const showDBQuery = `SELECT link FROM notices WHERE category = 'LANGUAGE';`;
   const languageLinks = (await selectQuery<NotiLink[]>(showDBQuery)).map(
     (language) => language.link,
   );
@@ -37,26 +60,14 @@ export const saveLanguageNoticeToDB = async () => {
     if (languageLinks.includes(notice)) continue;
 
     const result = await noticeContentCrawling(notice);
-    if (result.path === '') {
+    if (result.link === '') {
       notificationToSlack(`${notice} 어학 콘텐츠 크롤링 실패`);
       continue;
     }
 
-    const query =
-      'INSERT INTO 어학공지 (title, link, upload_date, author, rep_yn, category) VALUES (?,?,?,?,?,?)';
-    const values = [
-      result.title,
-      result.path,
-      result.date,
-      result.author,
-      false,
-      'LANGUAGE',
-    ];
-
-    await db.execute(query, values);
-    newNoticeTitle.push(result.title);
+    const res = await saveNotice(result);
+    if (res) newNoticeTitle.push(result.title);
   }
 
-  // await Promise.all(savePromises);
   return newNoticeTitle;
 };
