2fdcd53d71705da1087fe567f82b7dc9ea97ca27
Lee sang Yeop <mma7710@naver.com>
2023-12-01 02:47:05 +0900

    fix(cronNoticeCrawling): 공지사항 크롤링과 비교과 크롤링을 다른 함수로 세분화 학과 공지사항에 리소스가 크기에 공지사항 크롤링과 나머지 비교과 크롤링을 나누어 크롤링하도록 변경 추가적으로 에러 핸들링을 추가하여 에러가 발생하면 다시 함수를 실행하도록 수정 transaction을 이용해 함수실행 도중 에러발생 시 rollback 하도록 설정 (공지사항 구독한 유저에게 push 알림을 주기 위함)

--- src/hooks/cronNoticeCrawling.ts
+++ src/hooks/cronNoticeCrawling.ts
@@ -6,6 +6,7 @@ import {
   saveSchoolNoticeToDB,
   saveWhalebeToDB,
 } from '@db/data/noticeHandler';
+import db from '@db/index';
 import cron from 'node-cron';
 import notificationToSlack from 'src/hooks/notificateToSlack';
 
@@ -13,22 +14,46 @@ const pushToUsers = async (pushNotiToUserLists: PushNoti) => {
   let pushedUserCount = ``;
   for (const key in pushNotiToUserLists) {
     const count = await pushNotification(key, pushNotiToUserLists[key]);
-    pushedUserCount += `${key} ${count}명 알림 완료\n`;
+    if (!count) continue;
+    pushedUserCount += `학과번호:${key}, ${count}명 알림 완료\n`;
   }
   if (pushedUserCount.length !== 0) notificationToSlack(pushedUserCount);
 };
 
+const cronNoticeCrawling = async () => {
+  const connection = await db.getConnection();
+  try {
+    connection.beginTransaction();
+    const pushNotiToUserLists = await saveMajorNoticeToDB();
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
+    const day = today.getDate();
+    notificationToSlack(`${year}-${month}-${day} 크롤링 완료`);
+    pushToUsers(pushNotiToUserLists);
+    await connection.commit();
+  } catch (error) {
+    await connection.rollback();
+    notificationToSlack(error.message);
+    cronNoticeCrawling();
+  }
+};
+
+const cronExtracurricularCrawling = async () => {
+  try {
+    await saveSchoolNoticeToDB();
+    await saveLanguageNoticeToDB();
+    await saveWhalebeToDB();
+  } catch (error) {
+    notificationToSlack(error.message);
+    cronExtracurricularCrawling();
+  }
+};
+
 cron.schedule('0 0-9 * * 1-5', async () => {
-  notificationToSlack('크롤링 동작 시작!');
-  const pushNotiToUserLists = await saveMajorNoticeToDB();
-  await saveSchoolNoticeToDB();
-  await saveLanguageNoticeToDB();
-  await saveWhalebeToDB();
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1; // 월은 0부터 시작하므로 1을 더해줍니다.
-  const day = today.getDate();
-  notificationToSlack(`${year}-${month}-${day} 크롤링 완료`);
-  console.log(`${year}-${month}-${day} 크롤링 완료`);
-  pushToUsers(pushNotiToUserLists);
+  cronNoticeCrawling();
+});
+
+cron.schedule('0 0-9 * * 1-5', async () => {
+  cronExtracurricularCrawling();
 });
