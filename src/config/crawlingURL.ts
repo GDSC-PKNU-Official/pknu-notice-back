b3a5be7c0b1eed6566c4b16da8765125b28cc6c1
Lee sang Yeop <mma7710@naver.com>
2023-12-01 15:22:28 +0900

    feat(crawlingURL): 채용공지를 크롤링할 URL 추가

--- src/config/crawlingURL.ts
+++ src/config/crawlingURL.ts
@@ -3,4 +3,8 @@ export const PKNU_URL = {
     'https://www.pknu.ac.kr/main/163?pageIndex=1&bbsId=2&searchCondition=title&searchKeyword=교환학생&cd=',
   LANGUAGE_NOTICE_LANGUAGE_TRANING:
     'https://www.pknu.ac.kr/main/163?pageIndex=1&bbsId=2&searchCondition=title&searchKeyword=어학연수&cd=',
+  RECRUIT_NOTICE(pageIndex: number) {
+    return `https://pknujob.pknu.ac.kr/main/37?pageIndex=${pageIndex}&bbsId=11&searchCondition=0&strDt=&endDt=&searchKeyword=`;
+  },
+  RECRUIT_NOTICE_HOSTLINK: 'https://pknujob.pknu.ac.kr/main/37',
 };
