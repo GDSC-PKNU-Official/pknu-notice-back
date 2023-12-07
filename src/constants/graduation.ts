4cd91be617a21f304d0e667eaa0a72a99cfc502a
hwinkr <dnddl8280@naver.com>
2023-12-07 16:35:44 +0900

    chore(change datta): 학과 홈페이지 변경된 정보 반영

--- src/constants/graduation.ts
+++ src/constants/graduation.ts
@@ -10,10 +10,10 @@ export const EXCEPTIONAL_GRADUATION_KEYWORDS: Keywords = {
   금속공학전공: '졸업 요건',
   양식응용생명과학전공: '졸업 요건',
   냉동공조공학전공: '졸업매뉴얼',
-  언론정보전공: '졸업 요건',
+  해양수산경영학전공: '졸업사정',
 };
 
-export const EXCEPTIONAL_SEARCH_KEYWORDS: Keywords = {
+export const EXCEPTIONAL_NOTICE_PAGE_KEYWORDS: Keywords = {
   법학과: '졸업관련게시판',
   경영학부: '졸업 안내',
   조선해양시스템공학전공: '학부 규정',
@@ -22,8 +22,6 @@ export const EXCEPTIONAL_SEARCH_KEYWORDS: Keywords = {
 };
 
 export const EXCEPTIONAL_GRADUATION_LINKS: Keywords = {
-  해양수산경영학전공:
-    'https://mbe.pknu.ac.kr/view.do?no=504&idx=10414&view=view&pageIndex=1&sv=A&sw=%EC%A1%B8%EC%97%85%EC%9A%94%EA%B1%B4',
   환경대기과학전공:
     'https://icms.pknu.ac.kr/envatm/2268?action=view&no=9918655',
   공간정보시스템공학전공:
@@ -34,7 +32,6 @@ export const DOMAIN_TO_CMS_DEPARTMENTS = [
   '사학과',
   '일어일문학부',
   '정치외교학과',
-  '기계시스템공학전공',
 ];
 
 export const GRADUATION_IN_ARCHIVE = [
@@ -45,8 +42,6 @@ export const GRADUATION_IN_ARCHIVE = [
 
 export const GRADUATION_IN_NOTICE_A: Keywords = {
   화학공학과: 'chemeng',
-  생태공학전공: 'ecoeng',
-  재료공학전공: '/materials',
 };
 
 export const GRADUATION_IN_NOTICE_B = [
@@ -65,6 +60,7 @@ export const GRADUATION_IN_NOTICE_B = [
   '건축학전공',
   '식품영양학전공',
   '자원생물학전공',
+  '재료공학전공',
   '수산생명의학과',
   '해양학전공',
   '해양공학과',
@@ -74,4 +70,6 @@ export const GRADUATION_IN_NOTICE_B = [
   '경찰범죄심리학전공',
   '기계조선에너지시스템공학전공',
   '양식응용생명과학전공',
+  '생태공학전공',
+  '해양수산경영학전공',
 ];
