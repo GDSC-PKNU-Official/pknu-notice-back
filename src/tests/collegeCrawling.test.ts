0ae7bf9f719c4aee0a9821b81c6e38df074a4826
Lee sang Yeop <mma7710@naver.com>
2023-12-20 11:38:26 +0900

    test(collegeCrawling): 동작하지 않는 테스트코드 삭제

--- src/tests/collegeCrawling.test.ts
+++
@@ -1,6 +0,0 @@
-import { collegeCrawling } from '@crawling/collegeCrawling';
-
-test.skip('단과대학 크롤링 테스트', async () => {
-  const collegeList = await collegeCrawling();
-  expect(collegeList).toHaveLength(10);
-});
